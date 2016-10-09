5a6449e37e92fbb791f4effe0d59e6c0e64f5617
Shubham chawla <shubhamchawla00@gmail.com>
2016-10-09 14:59:07 +0530

    Initial Commit

---
+++ HaptikTask/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HaptikTask
+//
+//  Created by Shubham chawla on 10/9/16.
+//  Copyright © 2016 Shubham Chawla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HaptikTask/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HaptikTask
+//
+//  Created by Shubham chawla on 10/9/16.
+//  Copyright © 2016 Shubham Chawla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
